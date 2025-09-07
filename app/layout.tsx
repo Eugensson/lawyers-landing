a08dd3ceb443ef28998857032351b880b3cf1c22
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-07 14:36:00 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Playfair_Display, Open_Sans } from "next/font/google";
+
+import "./globals.css";
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Lawyers",
+    template: "%s | Lawyers",
+  },
+  description:
+    "Trusted law firm providing expert legal services, professional advice, and reliable representation for individuals and businesses.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${playfairDisplay.variable} ${openSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
